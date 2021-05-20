292802583695519377b8274931408db49e18049e
Matthew Lein <matthewlein@hotmail.com>
2021-05-19 21:16:12 -0500

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,44 @@
-export * from './lib/async';
-export * from './lib/number';
+#!/usr/bin/env node
+
+import fs from 'fs';
+import _ from 'lodash';
+
+const args = process.argv.slice(2);
+const filePath = args[0];
+const outputPath = filePath.replace('.json', '.sorted.json');
+
+console.log('Sorting collection file: ', filePath);
+
+const rawdata = fs.readFileSync(filePath, 'utf8');
+const json = JSON.parse(rawdata);
+
+interface PostmanRequest {
+  name: string;
+  request: {
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  };
+}
+
+const requests = json.item as PostmanRequest[];
+const verbMap = {
+  GET: 0,
+  POST: 1,
+  PUT: 2,
+  DELETE: 3,
+};
+
+const requestsSorted = _.sortBy(requests, (request: PostmanRequest) => {
+  return [request.name, verbMap[request.request.method]];
+});
+
+const newJson = {
+  ...json,
+  item: requestsSorted,
+};
+
+fs.writeFileSync(
+  filePath.replace('.json', '.sorted.json'),
+  JSON.stringify(newJson)
+);
+
+console.log(`Sorting Successful!\nSorted collection saved to: ${outputPath}`);
